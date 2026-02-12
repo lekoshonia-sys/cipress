6bb35fcf470b7d757bc8e9a8d0358310d89d0029
Leko Shonia <lekoshonia@gmail.com>
2026-02-12 18:03:16 +0400

    homework2

---
+++ cypress/e2e/homework2.cy.js
@@ -0,0 +1,45 @@
+describe('login informaation', () => {
+  it('logini', () => {
+    cy.visit('https://automationteststore.com/index.php?rt=account/login')
+
+cy.get('#loginFrm_loginname').type('leko53')
+cy.get('#loginFrm_password').type('nanikodadu12')
+cy.get('button[title="Login"]').click()
+cy.contains("My Account").should("be.visible")
+ 
+it('adit/change', () => {
+cy.visit ('https://automationteststore.com/index.php?rt=account/edit')
+cy.get('#AccountFrm_firstname').clear().type('levcho')
+cy.get('#AccountFrm_lastname').clear().type('mesxi')
+cy.get('#AccountFrm_email').clear().type('zevsius53@gmail.com')
+cy.get('#AccountFrm_telephone').clear().type('591454564')
+cy.get('button[title="Continue"]').click()
+cy.contains("Your account has been successfully updated.").should("be.visible")})
+cy.visit('https://automationteststore.com/index.php?rt=account/password')
+cy.get('#PasswordFrm_current_password').type('nanikodadu12')
+cy.get('#PasswordFrm_password').type('bakara12')
+cy.get('#PasswordFrm_confirm').type('bakara12')
+cy.get('button[title="Continue"]').click()
+cy.contains("Your password has been successfully updated.").should("be.visible")
+
+
+})
+
+
+})
+ 
+
+ 
+
+ 
+
+
+
+
+    
+  
+
+
+
+
+ 
